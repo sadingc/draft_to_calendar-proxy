ac0367315b8125a6b3dba4c896d69a6c1b288028
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-06-06 09:22:26 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,3 @@
-api_hash = "e3ce8145aa657c2a4cc5cf0f7183e476"
-api_id = 13233271
-bot_token = "5156350143:AAGn8qyD41Le4gnPlOzjj8BWuFNiFrBqGz4"
-bot_admin_group = -1001505671370
+api_hash = "c6ef54eb0cdd29441ae33921531cb4e8"
+api_id = 19919438
+bot_token = "5377296671:AAE5-uj9Ocmokz4TrVld63eHxVIGjKILqro"
